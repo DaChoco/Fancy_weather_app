f42aea8ed7a871b4e1c6bb799f7905d2a467cd61
DaChoko <Mailechokoe2@gmail.com>
2024-12-13 16:24:52 +0200

    testing with adding bg images on search depending on weather conditions

--- app.js
+++ app.js
@@ -6,15 +6,37 @@ const sidebar = document.getElementById('sidebar');
 
 const current_locale = document.querySelectorAll('.current-location')
 const current_temperature = document.querySelectorAll('.current-temp')
-const current_conditions = document.getElementById('condition')
+const current_conditions = document.querySelectorAll('.current-conditions')
 const feelslike = document.getElementById('feel-like')
 const highlow = document.getElementById('high-low')
 
+const currentTime = document.getElementById('time-now')
+
+const location_widgets = document.getElementById('location-widget-main');
+const weather_icon = document.getElementById('Weather-icon');
+
+const main_pageBG = document.querySelector('main');
+
 let arrayTemps = []; //for each day of this week
 
+function backgroundWeather(condition){
+    if (condition == "Partly Cloudy"){
+        location_widgets.classList.add('cloudy-day')
+        main_pageBG.classList.add('cloudy-day');
+
+        
+    }
+    else if (condition == "Clear" || condition == "Sunny"){
+        console.log(condiiton);
+        console.log('Clear skies')
+        location_widgets.classList.remove('cloudy-day')
+        main_pageBG.classList.remove('cloudy-day')
+    }
+}
 
 
-//this function extracts the current temperatire, the current high, current low, and current conditions
+
+//this function extracts the current temperature, the current high, current low, and current conditions
 const getWeather = async (location) =>{
     const url = `${config.MY_API_URL}/forecast.json?key=${config.MY_API_KEY}&q=${location}&days=7&aqi=no&alerts=no`;
 
@@ -32,10 +54,11 @@ const getWeather = async (location) =>{
         const feel = data.current.feelslike_c;
 
         const local_time = data.location.localtime;
+        const my_icon = data.current.condition.icon;
 
         console.log(feel);
     
-        return {current_temp, high_temp, low_temp, conditions, feel, local_time};
+        return {current_temp, high_temp, low_temp, conditions, feel, local_time, my_icon};
 
     } catch (error) {
             console.error('Error fetching weather data:', error);
@@ -51,10 +74,19 @@ window.addEventListener('DOMContentLoaded', function(){
             let location = searchBar.value;
 
             try{
-                const {current_temp, high_temp, low_temp, conditions, feel, local_time} = await getWeather(location);
+                const {current_temp, high_temp, low_temp, conditions, feel, local_time, my_icon} = await getWeather(location);
+
+                backgroundWeather(conditions);
 
                 console.log(`Current Temp: ${current_temp} Conditions: ${conditions} High: ${high_temp} Low: ${low_temp} Feels like ${feel}`); 
                 //making sure we extracted the information
+
+                feelslike.textContent = `Feels like: ${feel}`;
+                highlow.textContent = `H: ${high_temp}°C L: ${low_temp}°C `
+                currentTime.textContent =  local_time;
+                weather_icon.src = my_icon;
+
+                //--For each, seperating them to un clutter my code
 
                 current_locale.forEach(function(e){
                     e.textContent = location;
@@ -63,9 +95,10 @@ window.addEventListener('DOMContentLoaded', function(){
                 current_temperature.forEach(function(item){
                     item.textContent = current_temp;
                 })
-                feelslike.textContent = `Feels like: ${feel}`;
-                current_conditions.textContent = conditions;
-                highlow.textContent = `H: ${high_temp}°C L: ${low_temp}°C `
+
+                current_conditions.forEach(function(e){
+                    e.textContent = conditions
+                })
 
 
             }catch (error) { console.error('Error fetching weather data:', error)};
