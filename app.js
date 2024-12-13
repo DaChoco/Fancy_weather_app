dad3ce0a8bcc19a5eda51ab5614c2d146fb47f44
DaChoko <Mailechokoe2@gmail.com>
2024-12-13 14:59:40 +0200

    making it so that on search more html elements change

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ let searchBar = document.getElementById('search-bar');
 
 const sidebar = document.getElementById('sidebar');
 
+const current_locale = document.querySelectorAll('.current-location')
+const current_temperature = document.querySelectorAll('.current-temp')
+const current_conditions = document.getElementById('condition')
+const feelslike = document.getElementById('feel-like')
+const highlow = document.getElementById('high-low')
+
+let arrayTemps = []; //for each day of this week
+
 
 
 //this function extracts the current temperatire, the current high, current low, and current conditions
@@ -16,11 +24,18 @@ const getWeather = async (location) =>{
         console.log(data);
 
         const current_temp = data.current.temp_c;
-        const high_temp = data.forecast.forecastday[0].day.max_temp_c;
-        const low_temp = data.forecast.forecastday[0].day.min_temp_c;
-        const conditions = data.current.condition.text;
+
+        const high_temp = data.forecast.forecastday[0].day.maxtemp_c;
+        const low_temp = data.forecast.forecastday[0].day.mintemp_c;
     
-        return {current_temp, high_temp, low_temp, conditions};
+        const conditions = data.current.condition.text;
+        const feel = data.current.feelslike_c;
+
+        const local_time = data.location.localtime;
+
+        console.log(feel);
+    
+        return {current_temp, high_temp, low_temp, conditions, feel, local_time};
 
     } catch (error) {
             console.error('Error fetching weather data:', error);
@@ -36,9 +51,22 @@ window.addEventListener('DOMContentLoaded', function(){
             let location = searchBar.value;
 
             try{
-                const {current_temp, high_temp, low_temp, conditions} = await getWeather(location);
+                const {current_temp, high_temp, low_temp, conditions, feel, local_time} = await getWeather(location);
 
-                console.log(`The current temp is ${current_temp} while the conditions are ${conditions}`);
+                console.log(`Current Temp: ${current_temp} Conditions: ${conditions} High: ${high_temp} Low: ${low_temp} Feels like ${feel}`); 
+                //making sure we extracted the information
+
+                current_locale.forEach(function(e){
+                    e.textContent = location;
+                
+                })
+                current_temperature.forEach(function(item){
+                    item.textContent = current_temp;
+                })
+                feelslike.textContent = `Feels like: ${feel}`;
+                current_conditions.textContent = conditions;
+                highlow.textContent = `H: ${high_temp}°C L: ${low_temp}°C `
+
 
             }catch (error) { console.error('Error fetching weather data:', error)};
             
