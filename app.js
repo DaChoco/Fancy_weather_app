b41ebb093bb728829ce51d09a89fe09ad42661d1
DaChoko <Mailechokoe2@gmail.com>
2024-12-14 16:48:56 +0200

    caching my elements

--- app.js
+++ app.js
@@ -1,62 +1,56 @@
 import { config } from "./config.js";
 
-let searchBar = document.getElementById('search-bar');
+const elements = { //cache everything once this site loads
+    searchBar: document.getElementById('search-bar'),
+    current_locale: document.querySelectorAll('.current-location'),
+    current_temperature: document.querySelectorAll('.current-temp'),
+    current_conditions: document.querySelectorAll('.current-conditions'),
+    feelslike: document.getElementById('feel-like'),
+    highlow: document.getElementById('high-low'),
+    dropdown: document.querySelector('.dropsearch'),
+    currentTime: document.getElementById('time-now'),
+    location_widgets: document.getElementById('location-widget-main'),
+    weather_icon: document.getElementById('Weather-icon'),
+    main_pageBG: document.querySelector('main')
+}
 
-const sidebar = document.getElementById('sidebar');
 
-const current_locale = document.querySelectorAll('.current-location')
-const current_temperature = document.querySelectorAll('.current-temp')
-const current_conditions = document.querySelectorAll('.current-conditions')
-const feelslike = document.getElementById('feel-like')
-const highlow = document.getElementById('high-low')
-
-const dropdown = document.querySelector('.dropsearch')
-
-const currentTime = document.getElementById('time-now')
-
-const location_widgets = document.getElementById('location-widget-main');
-const weather_icon = document.getElementById('Weather-icon');
-
-const main_pageBG = document.querySelector('main');
-
-const listSelect = document.querySelectorAll('li')
 
 let arrayTemps = []; //for each day of this week
 
 function backgroundWeather(condition){
     if (condition == "Moderate rain at times" || condition == "Cloudy"){
-        location_widgets.classList.add('cloudy-day')
-        main_pageBG.classList.add('cloudy-day');
+        elements.location_widgets.classList.add('cloudy-day')
+        elements.main_pageBG.classList.add('cloudy-day');
 
         //removals
-        location_widgets.classList.remove('not-as-cloudy-day')
-        main_pageBG.classList.remove('not-as-cloudy-day')
-        location_widgets.classList.remove('not-cloudy-day')
-        main_pageBG.classList.remove('not-cloudy-day')
+        elements.location_widgets.classList.remove('not-as-cloudy-day')
+        elements.main_pageBG.classList.remove('not-as-cloudy-day')
+        elements.location_widgets.classList.remove('not-cloudy-day')
+        elements.main_pageBG.classList.remove('not-cloudy-day')
     }
     else if(condition == "Partly cloudy"){
-        location_widgets.classList.add('not-as-cloudy-day')
-        main_pageBG.classList.add('not-as-cloudy-day')
+        elements.location_widgets.classList.add('not-as-cloudy-day')
+        elements.main_pageBG.classList.add('not-as-cloudy-day')
 
         //removals
-        location_widgets.classList.remove('cloudy-day')
-        main_pageBG.classList.remove('cloudy-day')
-        location_widgets.classList.remove('not-cloudy-day')
-        main_pageBG.classList.remove('not-cloudy-day')
+        elements.location_widgets.classList.remove('cloudy-day')
+        elements.main_pageBG.classList.remove('cloudy-day')
+        elements.location_widgets.classList.remove('not-cloudy-day')
+        elements.main_pageBG.classList.remove('not-cloudy-day')
     }
     else if (condition == "Clear" || condition == "Sunny"){
-        location_widgets.classList.add('not-cloudy-day')
-        main_pageBG.classList.add('not-cloudy-day')
+        elements.location_widgets.classList.add('not-cloudy-day')
+        elements.main_pageBG.classList.add('not-cloudy-day')
 
         //removals
-        location_widgets.classList.remove('cloudy-day')
-        main_pageBG.classList.remove('cloudy-day')
-        location_widgets.classList.remove('not-as-cloudy-day')
-        main_pageBG.classList.remove('not-as-cloudy-day')
+        elements.location_widgets.classList.remove('cloudy-day')
+        elements.main_pageBG.classList.remove('cloudy-day')
+        elements.location_widgets.classList.remove('not-as-cloudy-day')
+        elements.main_pageBG.classList.remove('not-as-cloudy-day')
     }
 }
-
-
+ 
 
 //this function extracts the current temperature, the current high, current low, and current conditions
 const getWeather = async (location) =>{
@@ -87,11 +81,24 @@ const getWeather = async (location) =>{
      
 } 
 
+const getMyLocation = async () => {
+
+    const url = "http://ip-api.com/json/";
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    console.log(data.city);
+
+    return data.city;
+
+}
+
 window.addEventListener('DOMContentLoaded', function(){
 
-    searchBar.onkeyup = function(){ //auto complete
+    elements.searchBar.onkeyup = function(){ //auto complete
         let result = [];
-        let input = searchBar.value
+        let input = elements.searchBar.value
 
         
         if (input.length){
@@ -103,9 +110,9 @@ window.addEventListener('DOMContentLoaded', function(){
 
     }
 
-    searchBar.addEventListener('keydown', async function(e){
+    elements.searchBar.addEventListener('keydown', async function(e){
         if (e.key === 'Enter'){
-            let location = searchBar.value;
+            let location = elements.searchBar.value;
 
             try{
                 const {current_temp, high_temp, low_temp, conditions, feel, local_time, my_icon} = await getWeather(location);
@@ -115,24 +122,26 @@ window.addEventListener('DOMContentLoaded', function(){
                 console.log(`Current Temp: ${current_temp} Conditions: ${conditions} High: ${high_temp} Low: ${low_temp} Feels like ${feel}`); 
                 //making sure we extracted the information
 
-                feelslike.textContent = `Feels like: ${feel}°C`;
-                highlow.textContent = `H: ${high_temp}°C L: ${low_temp}°C `
-                currentTime.textContent =  local_time;
-                weather_icon.src = my_icon;
+                elements.feelslike.textContent = `Feels like: ${feel}°C`;
+                elements.highlow.textContent = `H: ${high_temp}°C L: ${low_temp}°C `
+                elements.currentTime.textContent =  local_time;
+                elements.weather_icon.src = my_icon;
 
                 //--For each, seperating them to un clutter my code
 
-                current_locale.forEach(function(e){
+                elements.current_locale.forEach(function(e){
                     e.textContent = location;
                 
                 })
-                current_temperature.forEach(function(item){
+                elements.current_temperature.forEach(function(item){
                     item.textContent = `${current_temp}°C`;
                 })
 
-                current_conditions.forEach(function(e){
+                elements.current_conditions.forEach(function(e){
                     e.textContent = conditions
                 })
+
+                elements.searchBar.value = "";
 
 
             }catch (error) { console.error('Error fetching weather data:', error)};
@@ -150,11 +159,11 @@ function displayResults(result){
     let content = "<ul>"
 
     result.forEach((list)=>{
-        content += `<li><h5>${list.name}, ${list.region}, ${list.country}</h5></li>`
+        content += `<li><h5>${list.name}, ${list.country}</h5></li>`
     })
 
     content += "</ul>"
-    dropdown.innerHTML = content;
+    elements.dropdown.innerHTML = content;
 
     fillautoSearch()
 }
@@ -177,28 +186,17 @@ const searchAutoComplete = async (location_search) =>{
 
 function fillautoSearch(){
 
-    listSelect.forEach(function(e){
-
-        e.addEventListener("click", function(){
-            searchBar.value = e.textContent;
-        })
-        
+    elements.dropdown.addEventListener('click', function(e){
+        if (e.target && e.target.tagName === 'LI'){
+            elements.searchBar.value = e.target.textContent;
+            elements.dropdown.innerHTML = "";
+            
+        }
     })
     
 }
 
-const getMyLocation = async () => {
 
-    const url = "http://ip-api.com/json/";
-
-    const response = await fetch(url);
-    const data = await response.json();
-
-    console.log(data.city);
-
-    return data.city;
-
-}
 
 window.addEventListener("load", async function(){
     let DefaultLocation = await getMyLocation();
@@ -210,27 +208,27 @@ window.addEventListener("load", async function(){
         backgroundWeather(conditions);
         //making sure we extracted the information
 
-        feelslike.textContent = `Feels like: ${feel} °C`;
-        highlow.textContent = `H: ${high_temp} °C L: ${low_temp} °C `
-        currentTime.textContent =  local_time;
-        weather_icon.src = my_icon;
+        elements.feelslike.textContent = `Feels like: ${feel} °C`;
+        elements.highlow.textContent = `H: ${high_temp} °C L: ${low_temp} °C `
+        elements.currentTime.textContent =  local_time;
+        elements.weather_icon.src = my_icon;
 
         //--For each, seperating them to un clutter my code
 
-        current_locale.forEach(function(e){
+        elements.current_locale.forEach(function(e){
             e.textContent = DefaultLocation;
         
         })
-        current_temperature.forEach(function(item){
-            item.textContent = `${current_temp}°C`;
+        elements.current_temperature.forEach(function(e){
+            e.textContent = `${current_temp}°C`;
         })
 
-        current_conditions.forEach(function(e){
-            e.textContent = conditions
+        elements.current_conditions.forEach(function(e){
+            e.textContent = conditions;
         })
 
 
-    }catch (error) { console.error('Error fetching weather data:', error)};
+    }catch (error) {console.error('Error fetching weather data:', error)};
 
 
 })
